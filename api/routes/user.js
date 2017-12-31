0d309006bea9e7e32a6a590d8a0aa52a87ce744e
Yev <evlogosha@gmail.com>
2017-12-31 17:09:12 +0200

    add token and return to client

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
+// create new user
 app.post('/signup', (req, res) => {
     User.find({email: req.body.email})
     .exec()
@@ -41,6 +43,49 @@ app.post('/signup', (req, res) => {
             })
         }
     })
+})
+
+// creat token if user exists (log user in)
+app.post('/login', (req, res) => {
+    User.find({ email: req.body.email })
+    .exec()
+    .then(user => {
+        if(user.length < 1) {
+            return res.status(401).json({
+                message: 'Auth Failed'
+            })
+        }
+        bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+            if(err) {
+                return res.status(401).json({
+                    message: 'Auth Failed'
+                })
+            }
+            if (result) {
+                const token = jwt.sign(
+                {
+                    email: user[0].email,
+                    userID: user[0]._id
+                }, 
+                process.env.JWT_KEY,
+                {
+                    expiresIn: '1h'
+                }
+             )
+                return res.status(200).json({
+                    message: 'Auth Successful',
+                    token: token
+                })
+            }
+            res.status(401).json({
+                message: 'Auth Failed'
+            })
+        })
+    })
+    .catch((err => {
+        console.log(err);
+        res.status(500).json({ error: err })
+    }))
 })
 
 app.delete('/:userID', (req, res) => {
